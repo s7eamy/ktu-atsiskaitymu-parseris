22a51774161793d1ee18c2b54699dbe8613ea6fe
s7eamy <ragulis.aurimas@gmail.com>
2025-02-04 10:04:14 +0200

    feat: add weight attribute to assignment

--- assignment.py
+++ assignment.py
@@ -1,8 +1,6 @@
 class Assignment:
-	def __init__(self, name, assign_date, due_date):
+	def __init__(self, name, assign_date, due_date, weight):
 		self.name = name
 		self.assign_date = assign_date
 		self.due_date = due_date
-
-	def __str__(self):
-		return f"{self.name} assigned on week {self.assign_date}, due on week {self.due_date}"
+		self.weight = weight
